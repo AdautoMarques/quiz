ee107a84a1e0b44f73b5e9b8f4bcce0cfe7c0c7b
Adauto Marques <adautomarques937@gmail.com>
2025-01-17 10:08:39 -0300

    Projeto de quiz sobre o judaismo

---
+++ src/components/QuestionItem.tsx
@@ -0,0 +1,58 @@
+import { Question } from "@/types/Question";
+import { useState } from "react";
+
+type Props = {
+  question: Question;
+  count: number;
+  onAnswer: (answer: number) => void;
+};
+
+export const QuestionItem = ({ question, count, onAnswer }: Props) => {
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
+  const checkQuestion = (key: number) => {
+    if (selectedAnswer === null) {
+      setSelectedAnswer(key);
+
+      setTimeout(() => {
+        onAnswer(key);
+        setSelectedAnswer(null);
+      }, 1500);
+    }
+  };
+
+  return (
+    <div>
+      <div className="text-2xl font-bold mb-5">
+        {count}. {question.question}
+      </div>
+      <div>
+        {question.options.map((item, key) => (
+          <div
+            key={key}
+            className={`border border-stone-400 px-3 py-2 rounded-md text-lg mb-4  bg-stone-200 ${
+              selectedAnswer !== null
+                ? "cursor-auto "
+                : "hover:opacity-60 cursor-pointer"
+            }
+              ${
+                selectedAnswer !== null &&
+                selectedAnswer === question.answer &&
+                selectedAnswer === key &&
+                "bg-green-200 border-green-400"
+              }
+              ${
+                selectedAnswer !== null &&
+                selectedAnswer !== question.answer &&
+                selectedAnswer === key &&
+                "bg-red-200 border-red-400"
+              }
+            `}
+            onClick={() => checkQuestion(key)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/data/questions.ts
@@ -0,0 +1,41 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+  {
+    question: "Qual é o livro sagrado central do judaismo?",
+    options: ["Alcorão", "Novo Testamento", "Torá", "Bhagavad Gita"],
+    answer: 2,
+  },
+  {
+    question: "Qual é o símbolo mais reconhecido do judaísmo?",
+    options: ["Cruz", "Crescente e Estrela", "Estrela de Davi", "Peixe"],
+    answer: 2,
+  },
+  {
+    question: "Qual é o dia sagrado da semana para os judeus?",
+    options: ["Domingo", "Sexta-Feira", "Sábado", "Segunda-Feira"],
+    answer: 2,
+  },
+  {
+    question: "Qual é o idioma original dos textos sagrados judaicos?",
+    options: ["Latim", "Árabe", "Grego", "Hebraico"],
+    answer: 3,
+  },
+  {
+    question:
+      "Qual é a festividade judaica que celebra a libertação dos hebreus do Egito?",
+    options: ["Rosh Hashaná", "Pessach", "Yom Kippur", "Hanucá"],
+    answer: 1,
+  },
+  {
+    question:
+      "Qual é o alimento que simboliza a fuga apressada dos judeus do Egito?",
+    options: [
+      "Matzo (pão ázimo)",
+      "Matzá (pão ázimo)",
+      "Challah (pão trançado)",
+      "Kugel (prato de macarrão ou batata)",
+    ],
+    answer: 1,
+  },
+];
